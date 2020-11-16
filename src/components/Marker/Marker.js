b0e4590ef3b969c68b648a33d0b881d033499d57
Kaio Gabriel <kaio.gabrielsr@gmail.com>
2020-11-16 10:38:07 -0300

    fix: handle error when loading marker picture

--- src/components/Marker/Marker.js
+++ src/components/Marker/Marker.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './marker.css'
 
 function Marker({ restaurant, handleMarkerClick }) {
+  const [imgError, setImgError] = useState(false)
+
   return (
     <div className={`marker marker--${restaurant.type}`} data-testid="marker"
       onClick={() => handleMarkerClick(restaurant)}>
-      { restaurant.pictureURL ?
-      <img src={restaurant.pictureURL} className="marker__picture" alt="foto"/> :
-      <div className="marker__picture marker__picture--icon">
-        <div className="icon icon--leaf"></div>
-      </div>
+      { restaurant.pictureURL && !imgError 
+        ? <img src={restaurant.pictureURL} 
+            className="marker__picture" 
+            alt="foto"
+            onError={() => setImgError(true)}
+          />
+        : <div className="marker__picture marker__picture--icon">
+            <div className="icon icon--leaf"></div>
+          </div>
       }
     </div>
   )
